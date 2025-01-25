e5307b24edb97c006a9b1739ee9e5153efb09c9e
TackTeam-Chare <chare.auppachitwan@gmail.com>
2025-01-25 14:21:40 +0700

    add

--- next.config.js
+++ next.config.js
@@ -1,18 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  productionBrowserSourceMaps: false,
   reactStrictMode: true,
+  productionBrowserSourceMaps: false,
   env: {
     NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL,
     NEXT_PUBLIC_GOOGLE_MAPS_API_KEY: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
     NEXT_PUBLIC_LIFF_ID: process.env.NEXT_PUBLIC_LIFF_ID,
   },
   images: {
-    domains: [
-      'maps.googleapis.com',
-      'tailwindui.com',
-      'www.thaitravelcenter.com',
+    domains: ['maps.googleapis.com', 'tailwindui.com', 'www.thaitravelcenter.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'example.com',
+        pathname: '/images/**',
+      },
     ],
     unoptimized: true,
   },
